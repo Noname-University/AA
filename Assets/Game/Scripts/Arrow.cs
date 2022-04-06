cff590ba966edb43759082275d54d3e63f3feb52
Erdoğan IŞIK <erdogani0202@gmail.com>
2022-04-06 14:58:37 +0300

    Object movement added

---
+++ Assets/Game/Scripts/Arrow.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    private bool control;
+    void Update()
+    {
+        if (Input.touchCount>0)
+        {
+            control = true;
+        }
+
+        if (control)
+        {
+            transform.position +=new Vector3(0,Time.deltaTime * speed,0);
+           
+        }
+        
+       
+        
+            
+        
+    }
+    private void OnTriggerEnter(Collider other) {
+        transform.position +=new Vector3(0,Time.deltaTime * speed,0);
+
+    }
+}
