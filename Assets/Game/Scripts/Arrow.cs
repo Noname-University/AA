aaf1430abdc6480801f003e24e80747b2fbc8285
Erdoğan IŞIK <erdogani0202@gmail.com>
2022-04-06 16:07:17 +0300

    Arrow stop added

--- Assets/Game/Scripts/Arrow.cs
+++ Assets/Game/Scripts/Arrow.cs
@@ -7,20 +7,30 @@ public class Arrow : MonoBehaviour
     [SerializeField]
     private float speed;
     private bool control;
+    private float movement;
+    private Rigidbody rb;
+
+    private void Start() {
+        rb=GetComponent<Rigidbody>();
+    }
     void Update()
     {
+       
+        
         if (Input.touchCount>0)
         {
+            Debug.Log("asdasd");
             control = true;
         }
 
         if (control)
         {
-            transform.position +=new Vector3(0,Time.deltaTime * speed,0);
-           
+            rb.velocity =  new Vector3(0,speed*Time.deltaTime,0);
+            // transform.position +=new Vector3(0,Time.deltaTime * speed,0);
         }
 
     }
 
+    
     
 }
--- Assets/Game/Scripts/MainObject.cs
+++ Assets/Game/Scripts/MainObject.cs
@@ -6,11 +6,17 @@ public class MainObject : MonoBehaviour
 {
     [SerializeField]
     private float speed;
+    [SerializeField]
+    private GameObject arrow;
 
-    // Update is called once per frame
     void Update()
     {
         transform.Rotate(0,0,-(speed*Time.deltaTime));
     }
     
+    private void OnTriggerEnter(Collider other) {
+        Debug.Log("asdasd");
+        arrow.transform.parent=transform;
+        arrow.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePosition;
+    }
 }
